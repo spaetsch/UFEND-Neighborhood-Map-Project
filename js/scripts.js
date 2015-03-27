294c025bc93ce311ad19a3d391ceee381f7c0db4
spaetsch <spaetsch@gmail.com>
2015-03-27 15:38:28 -0700

    Refactor into MVVM

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,55 @@
-      	function initialize() {
-        	var mapOptions = {
-  			//center: { lat: -34.397, lng: 150.644},
-         	//zoom: 8
-  			center: { lat: 47.6374701, lng: -122.3578885}, //Queen Anne Seattle
-       		zoom: 14
-        	};
-        	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-      	}
-      	google.maps.event.addDomListener(window, 'load', initialize);
+// --------- MODEL ---------------
+
+var model = {
+  markers : {
+    houseMarker : new google.maps.Marker({
+      position: new google.maps.LatLng(47.635559, -122.372503),
+      title: 'New House',
+      category: "house"
+    }),
+
+    WFMarker : new google.maps.Marker({
+      position: new google.maps.LatLng(47.637004, -122.376816),
+      title: 'Whole Foods',
+      category: "grocery"
+    }),
+
+    TJMarker : new google.maps.Marker({
+      position: new google.maps.LatLng(47.636412, -122.356463),
+      title: "Trader Joe's",
+      category: "grocery"
+    })
+  }
+}
+
+// --------BINDER ---------------
+
+
+// ------- VIEW --------------
+
+
+function initialize() {
+
+  var mapOptions = {
+    center: { lat: 47.6374701, lng: -122.3578885}, //Queen Anne Seattle
+    zoom: 14
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  map.setOptions({draggableCursor:'url(http://maps.gstatic.com/mapfiles/openhand_8_8.cur),default'}); // trying to fix bug
+  map.setOptions({draggingCursor:'url(http://maps.gstatic.com/mapfiles/closedhand_8_8.cur),default'}); // trying to fix bug
+
+  //model.markers.houseMarker.setMap(map);
+  //model.markers.WFMarker.setMap(map);
+  //model.markers.TJMarker.setMap(map);
+
+  for (marker in model.markers){
+    model.markers[marker].setMap(map);
+  }
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
