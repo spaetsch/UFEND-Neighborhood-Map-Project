a54a6cdf8bef0c9e76fee23e6907f17a2fa6caaa
spaetsch <spaetsch@gmail.com>
2015-04-20 11:58:01 -0700

    Refactor to use anonymous function for geocoder callback

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@
 var markersModel = [
     {
       title: 'Whole Foods',
-      category: "grocery",
-      address: "2001 15th Ave. W., Seattle, WA", //add address to marker
-      marker : new google.maps.Marker({
-        position: new google.maps.LatLng(0,0),
-        icon: "img/pins/supermarket.png"
+      category: "grocery",                        
+      address: "2001 15th Ave. W., Seattle, WA",  // street address for use by geocoder
+      marker : new google.maps.Marker({           // google maps marker object
+        position: new google.maps.LatLng(0,0),    // set initial position to (0,0)
+        icon: "img/pins/supermarket.png"          // map icon by category
       })
     },
     {
       title: "Trader Joe's",
       category: "grocery",
-      address: "1916 Queen Anne Ave., Seattle, WA", //add address to marker
+      address: "1916 Queen Anne Ave., Seattle, WA", 
       marker : new google.maps.Marker({
         position: new google.maps.LatLng(0,0), 
         icon: "img/pins/supermarket.png"
@@ -22,7 +22,7 @@ var markersModel = [
     {
       title: "Safeway",
       category: "grocery",
-      address: "2100 Queen Anne Ave., Seattle, WA", //add address to marker
+      address: "2100 Queen Anne Ave., Seattle, WA", 
       marker : new google.maps.Marker({
         position: new google.maps.LatLng(0,0),
         icon: "img/pins/supermarket.png"
@@ -31,7 +31,7 @@ var markersModel = [
     {
       title: "The Seattle Grind",
       category: "coffee",
-      address: "1907 10th Ave. W., Seattle, WA", //add address to marker
+      address: "1907 10th Ave. W., Seattle, WA", 
       marker : new google.maps.Marker({
         position: new google.maps.LatLng(0,0),      
         icon: "img/pins/coffee.png",
@@ -40,7 +40,7 @@ var markersModel = [
     {
       title: "Bustle on Queen Anne",
       category: "coffee",
-      address: "535 W. McGraw St., Seattle, WA", //add address to marker
+      address: "535 W. McGraw St., Seattle, WA", 
       marker : new google.maps.Marker({
         position: new google.maps.LatLng(0,0),
         icon: "img/pins/coffee.png",
@@ -49,7 +49,7 @@ var markersModel = [
     {
       title: "5 Spot",
       category: "restaurant",
-      address: "1502 Queen Anne Ave N., Seattle, WA", //add address to marker
+      address: "1502 Queen Anne Ave N., Seattle, WA", 
       marker : new google.maps.Marker({
         position: new google.maps.LatLng(0,0),
         icon: "img/pins/restaurant.png",
@@ -58,7 +58,7 @@ var markersModel = [
     {
       title: "Homegrown",
       category: "restaurant",
-      address: "2201 Queen Anne Ave N., Seattle, WA", //add address to marker
+      address: "2201 Queen Anne Ave N., Seattle, WA", 
       marker : new google.maps.Marker({
         position: new google.maps.LatLng(0,0),
         icon: "img/pins/restaurant.png",
@@ -67,7 +67,7 @@ var markersModel = [
     {
       title: "West Howe Park",
       category: "park",
-      address: "1901 11th Ave. W., Seattle, WA", //add address to marker
+      address: "1901 11th Ave. W., Seattle, WA", 
       marker : new google.maps.Marker({
         position: new google.maps.LatLng(0,0),
         icon: "img/pins/park.png",
@@ -76,7 +76,7 @@ var markersModel = [
     {
       title: "Kinnear Park",
       category: "park",
-      address: "749-827 W. Olympic Pl., Seattle, WA", //add address to marker
+      address: "749-827 W. Olympic Pl., Seattle, WA", 
       marker : new google.maps.Marker({
         position: new google.maps.LatLng(0,0), 
         icon: "img/pins/park.png",
@@ -85,14 +85,13 @@ var markersModel = [
   ]
 
 
-// ------- VIEWMODEL --------------
-
+// -------------- VIEWMODEL ----------------
 
 var resultMarkers = function(members){
   var self = this;
 
   var mapOptions = {
-      center: new google.maps.LatLng(47.635930, -122.364991),//(47.6374701,-122.3578885),
+      center: new google.maps.LatLng(47.635930, -122.364991), //set map center in Queen Anne
       zoom: 15
     };
 
@@ -101,16 +100,19 @@ var resultMarkers = function(members){
   self.markers = ko.observableArray(members); 
   self.searchReq = ko.observable("");
 
-
-
-  self.clearMap = function (map) {
+  /*self.clearMap = function (map) {
       for (current in markersModel) {
         markersModel[current].marker.setMap(null);
       }
-    }
+    }*/
 
   self.filteredMarkers = ko.computed(function() {
-    self.clearMap();
+    //self.clearMap();
+
+    for (current in markersModel) {
+        markersModel[current].marker.setMap(null);
+      }
+
     return $.grep(members, function( a ) {
       if(a.title.toLowerCase().indexOf(self.searchReq().toLowerCase()) > -1 ||
           a.category.toLowerCase().indexOf(self.searchReq().toLowerCase()) > -1){
@@ -121,23 +123,10 @@ var resultMarkers = function(members){
       });
   });
   
-  self.geocoderCallback = function(marker){
-    return function(results, status) {  
-      markersModel[marker].marker.position = results[0].geometry.location;
-      markersModel[marker].marker.setMap(map);
-    }
-  };
-
-
-
   self.addAllMarkers = function(){  
-    
-
+    //iterate through all markers in the model
     for (current in markersModel){
-
-
-      
-        
+        //add event listener to each map marker to trigger the corresponding infowindow on click
         google.maps.event.addListener(markersModel[current].marker, 'click', function(innerCurrent) {
           return function(){
             var infowindow = new google.maps.InfoWindow({
@@ -148,21 +137,15 @@ var resultMarkers = function(members){
         }(current));
 
         geocoder = new google.maps.Geocoder();
-        geocoder.geocode({ 'address': markersModel[current].address }, self.geocoderCallback(current));
-      
-      }
-  };
-
-/*
-  self.mapInit = function(){
-  
-    // fixing bug in google code
-    map.setOptions({draggableCursor:'url(http://maps.gstatic.com/mapfiles/openhand_8_8.cur),default'}); 
-    map.setOptions({draggingCursor:'url(http://maps.gstatic.com/mapfiles/closedhand_8_8.cur),default'}); 
-
-    self.addAllMarkers();
-  };
-*/
+        //place marker on map using address to find LatLng with geocoder
+        geocoder.geocode({ 'address': markersModel[current].address }, function(current){
+          return function(results, status) {  
+            markersModel[current].marker.position = results[0].geometry.location;
+            markersModel[current].marker.setMap(map);
+          }
+        }(current));
+    }
+  }
 }
 
 var toggleBounce = function(currentMarker) {
@@ -171,7 +154,6 @@ var toggleBounce = function(currentMarker) {
   } else {
     currentMarker.marker.setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(function (){currentMarker.marker.setAnimation(null)}, 2800);
-
   }
 }
 
