154c7553ee13237ff196dda8c22f380aed4058be
spaetsch <spaetsch@gmail.com>
2015-04-29 15:38:47 -0700

    Working on error handling

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
+if(!window.google || !window.google.maps){
+      $('#map-container').text("Error: Google Maps data could not be loaded");
+      $('#map-list').text("Error: Google Maps data could not be loaded");
+}
+
 // --------- MODEL ---------------
-
-  if(!window.google || !window.google.maps){
-        $('#map-container').text("Error: Google Maps data could not be loaded");
-        $('#map-list').text("Error: Google Maps data could not be loaded");
-
-    }
  
 var markersModel = [
+
   {
     title: "5 Spot",
     category: "restaurant",
-    address: "1502 Queen Anne Ave N., Seattle, WA", 
-    phone:"(206) 285-7768",
-    marker : new google.maps.Marker({
-      position: new google.maps.LatLng(0,0),
-      icon: "img/pins/restaurant.png",
+    address: "1502 Queen Anne Ave N., Seattle, WA", // street address for use by Google Maps geocoder
+    phone:"(206) 285-7768",                         // phone number for use by Yelp API
+    status: ko.observable("OK"),
+    marker : new google.maps.Marker({               // google maps marker object
+      position: new google.maps.LatLng(0,0),          // set initial position to (0,0)
+      icon: "img/pins/restaurant.png"                 // map icon by category
     }) 
   },
   {
@@ -22,9 +23,10 @@ var markersModel = [
     category: "coffee",
     address: "535 W. McGraw St., Seattle, WA", 
     phone:"(206) 453-4285",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0),
-      icon: "img/pins/coffee.png",
+      icon: "img/pins/coffee.png"
     }) 
   },
    {
@@ -32,9 +34,21 @@ var markersModel = [
     category: "coffee",
     address: "2205 Queen Anne Ave N, Seattle, WA", 
     phone:"(206) 282-5313",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0),
-      icon: "img/pins/coffee.png",
+      icon: "img/pins/coffee.png"
+    }) 
+  },
+   {
+    title: "BAD ADDRESS DATA ",
+    category: "restaurant",
+    address: "plergh", // street address for use by Google Maps geocoder
+    phone:"(206) 285-7768",  
+    status: ko.observable("OK"),                       // phone number for use by Yelp API
+    marker : new google.maps.Marker({               // google maps marker object
+      position: new google.maps.LatLng(0,0),          // set initial position to (0,0)
+      icon: "img/pins/restaurant.png"                 // map icon by category
     }) 
   },
   {
@@ -42,9 +56,10 @@ var markersModel = [
     category: "restaurant",
     address: "2201 Queen Anne Ave N., Seattle, WA", 
     phone: "(206) 217-4745",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0),
-      icon: "img/pins/restaurant.png",
+      icon: "img/pins/restaurant.png"
     })
   },
   {
@@ -52,6 +67,7 @@ var markersModel = [
     category: "restaurant",
     address: "198 Nickerson St, Seattle, WA", 
     phone: "(206) 283-2665",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0), 
       icon: "img/pins/restaurant.png"
@@ -62,9 +78,10 @@ var markersModel = [
     category: "pub",
     address: "318 Nickerson St, Seattle, WA", 
     phone: "(206) 284-8819",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0),
-      icon: "img/pins/bar.png",
+      icon: "img/pins/bar.png"
     })
   },
   {
@@ -72,9 +89,10 @@ var markersModel = [
     category: "pub",
     address: "2125 Queen Anne Ave N, Seattle, WA", 
     phone: "(206) 283-4548",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0),
-      icon: "img/pins/bar.png",
+      icon: "img/pins/bar.png"
     })
   },
   {
@@ -82,9 +100,10 @@ var markersModel = [
     category: "coffee",
     address: "2128 Queen Anne Ave N, Seattle, WA", 
     phone:"(206) 780-5777",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0),
-      icon: "img/pins/coffee.png",
+      icon: "img/pins/coffee.png"
     }) 
   },
   {
@@ -92,9 +111,10 @@ var markersModel = [
     category: "pub",
     address: "1625 Queen Anne Ave N, Seattle, WA", 
     phone: "(206) 283-3900",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0),
-      icon: "img/pins/bar.png",
+      icon: "img/pins/bar.png"
     })
   },
   {
@@ -102,6 +122,7 @@ var markersModel = [
     category: "restaurant",
     address: "1903 10th Ave W, Seattle, WA", 
     phone: "(206) 708-6742",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0), 
       icon: "img/pins/restaurant.png"
@@ -112,6 +133,7 @@ var markersModel = [
     category: "grocery",
     address: "1916 Queen Anne Ave., Seattle, WA", 
     phone: "(206) 284-2546",
+    status: ko.observable("OK"),
     marker : new google.maps.Marker({
       position: new google.maps.LatLng(0,0), 
       icon: "img/pins/supermarket.png"
@@ -120,11 +142,12 @@ var markersModel = [
   {
     title: 'Whole Foods',
     category: "grocery",                        
-    address: "2001 15th Ave. W., Seattle, WA",  // street address for use by Google Maps geocoder
-    phone:"(206) 352-5440",                     // phone number for use by Yelp API
-    marker : new google.maps.Marker({           // google maps marker object
-      position: new google.maps.LatLng(0,0),      // set initial position to (0,0)
-      icon: "img/pins/supermarket.png"            // map icon by category
+    address: "2001 15th Ave. W., Seattle, WA",  
+    phone:"(206) 352-5440",    
+    status: ko.observable("OK"),                 
+    marker : new google.maps.Marker({           
+      position: new google.maps.LatLng(0,0),      
+      icon: "img/pins/supermarket.png"            
     })
   },
 ]
@@ -135,7 +158,7 @@ var resultMarkers = function(members){
   var self = this;
 
   self.mapOptions = {
-      center: new google.maps.LatLng(47.641437, -122.360834),//(47.635930, -122.364991), //set map center in Queen Anne
+      center: new google.maps.LatLng(47.641437, -122.360834), //set map center in Queen Anne
       zoom: 15
     };
 
@@ -148,18 +171,22 @@ var resultMarkers = function(members){
     //remove all markers from map
     for (current in markersModel) {
         markersModel[current].marker.setMap(null);
-      }
-    //place only markers that match search request
-    return $.grep(members, function( a ) {
+    }
     
+
+
+    //place only the markers that match search request
+    return $.grep(members, function( a ) { 
+   
       var titleSearch = a.title.toLowerCase().indexOf(self.searchReq().toLowerCase());
       var catSearch = a.category.toLowerCase().indexOf(self.searchReq().toLowerCase());   
-    
-      if( titleSearch > -1 || catSearch > -1){
+      if(titleSearch > -1 || catSearch > -1){
           a.marker.setMap(self.map);
         } 
-        return titleSearch > -1 || catSearch > -1;
-        });
+        return ((titleSearch > -1 || catSearch > -1) && a.status() === "OK")  
+  
+    });
+
   });
   
   self.addAllMarkers = function(){  
@@ -190,13 +217,28 @@ var resultMarkers = function(members){
         geocoder = new google.maps.Geocoder();
         //place marker on map using address to find LatLng with geocoder
         geocoder.geocode({ 'address': markersModel[current].address }, function(current){
-          return function(results, status) {  
-            markersModel[current].marker.position = results[0].geometry.location;
-            markersModel[current].marker.setMap(self.map);
+          return function(results, status) { 
+            if (status === "OK" || status === "OVER_QUERY_LIMIT"){
+              console.log("in IF OK: ", markersModel[current].title, "is", status);
+              markersModel[current].marker.position = results[0].geometry.location;
+              markersModel[current].marker.setMap(self.map);
+            } 
+            else{
+              console.log("in geocode else, markersModel[current].title is", markersModel[current].title);
+              console.log("status is",status)
+
+              markersModel[current].status("ERROR");
+
+            }
+           
           }
         }(current));
-    }//for loop
-  }//addAllMarkers
+    }//--for loop
+  }//--addAllMarkers
+
+
+ 
+
 
   //toggle bounce animation on click (data-binding)
   self.toggleBounce = function(currentMarker) {
